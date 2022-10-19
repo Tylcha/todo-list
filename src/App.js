0e323e2eb489a7bb7e1049ad88716d74271b9863
Talha Nazli <ttalhanazli@gmail.com>
2022-10-19 06:20:16 +0300

    add boostrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="d-flex justify-content-center align-items-center h-100" style='height: 100%;'>
+      <h1 className="h1">selam</h1>
       
     </div>
   );
